cc3f72b6f5cdcf629d3542f51c90ceb289522157
Gift Chimphonda <gift.chimphonda@outlook.com>
2022-01-18 11:54:25 +0200

    Brainstorm Session #3

--- 01_sqrt.py
+++ 01_sqrt.py
@@ -9,19 +9,22 @@ def sqrt(number):
     """
     start = 0
     end = number
-    guess = (start + end)/2
-    while guess**2!=number:
-        if guess**2 > number:
-            end = guess
-            guess = (start + end)/2
-        elif guess**2<number:
-            if (guess + 1)**2>number:
-                return guess
-            else:
-                start = guess
-                guess = (start + end)/2
+    guess = (start + end)//2
+    
+    
+def _sqrt(start,mid,end,target):
+    if mid**2 > target:
+        end = mid
+        mid = (start + end)//2
+        return sqrt()
+    elif mid**2<target:
+        if (mid + 1)**2 > target:
+            return mid
         else:
-            return guess
+            start = guess
+            mid = (start + end)//2
+    else:
+        return mid
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
