62f6acbf8a2b0b52ac7656ab4b1c2d3d38e04855
Gift Chimphonda <gift.chimphonda@outlook.com>
2022-01-18 11:33:37 +0200

    Brainstorm Session #2

--- 01_sqrt.py
+++ 01_sqrt.py
@@ -7,18 +7,21 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    guess = number/2
-    if guess**2>number:
-        guess -= floor((guess + (guess/2))/2)
-    else:
-        if guess**2==number:
-            return guess
-        else:
-            if guess**2<number and (guess+1)**2>number:
+    start = 0
+    end = number
+    guess = (start + end)/2
+    while guess**2!=number:
+        if guess**2 > number:
+            end = guess
+            guess = (start + end)/2
+        elif guess**2<number:
+            if (guess + 1)**2>number:
                 return guess
             else:
-                guess += floor((guess + (guess/2))/2)   
-    pass
+                start = guess
+                guess = (start + end)/2
+        else:
+            return guess
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
