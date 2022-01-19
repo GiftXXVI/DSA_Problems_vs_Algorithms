0ed12128fcedffadaad27e054a93139ce92bf2d9
GiftXXVI <gift.chimphonda@outlook.com>
2022-01-19 07:33:55 +0200

    Rotateted Array Brainstorm Session #1

--- 02_rotated_arr.py
+++ 02_rotated_arr.py
@@ -7,20 +7,36 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
-    
+    pivot = find_pivot(input_list, 0, len(input_list)-1)
+    print(pivot)
     pass
 
-def find_pivot():
-    pass
+
+def find_pivot(arr, start, end):
+    mid = (start+end)//2
+    if len(arr) > 1:
+        if arr[mid] > arr[mid+1]:
+            return mid
+        if arr[start] > arr[mid]:
+            end = mid
+        else:
+            start = mid+1
+        return find_pivot(arr[start:end], start, end)
+    else:
+        print(arr)
+        return None
+
 
 def _get_index():
     pass
+
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
         if element == number:
             return index
     return -1
+
 
 def test_function(test_case):
     input_list = test_case[0]
@@ -30,8 +46,15 @@ def test_function(test_case):
     else:
         print("Fail")
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+
+#test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+#test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+#test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+#test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+#test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+
+rotated_array_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 6)
+rotated_array_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 1)
+rotated_array_search([6, 7, 8, 1, 2, 3, 4], 8)
+rotated_array_search([6, 7, 8, 1, 2, 3, 4], 1)
+rotated_array_search([6, 7, 8, 1, 2, 3, 4], 10)
