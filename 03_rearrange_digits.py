4344624db0dd7086063ac21bbba4df3896728c57
GiftXXVI <gift.chimphonda@outlook.com>
2022-01-20 15:47:27 +0200

    Completed Rearrange Digits

--- 03_rearrange_digits.py
+++ 03_rearrange_digits.py
@@ -15,8 +15,8 @@ def heapsort(input, size):
     end = size - 1
     while end > 0:
         temp = input[end]
-        input[0] = input[end]
-        input[end] = temp
+        input[end] = input[0]
+        input[0] = temp
         end -= 1
         sift_down(input, 0, end)
     return input
@@ -36,17 +36,16 @@ def sift_down(input, start, end):
     while get_ileftchild(root) <= end:
         child = get_ileftchild(root)
         swap = root
-
         if input[swap] < input[child]:
             swap = child
         if child + 1 <= end and input[swap] < input[child + 1]:
             swap = child + 1
         if swap == root:
-            return
+            break
         else:
-            temp = input[root]
+            temp_s = input[swap]
             input[swap] = input[root]
-            input[root] = temp
+            input[root] = temp_s
             root = swap
     return input
 
@@ -61,12 +60,20 @@ def rearrange_digits(input_list):
        (int),(int): Two maximum sums
     """
     size = len(input_list)
-    sizes = (size // 2, size - (size // 2))
-    max_size = max(sizes)
-    print(size, sizes, max_size)
-    i = max_size
-    while i > 0:
-        i -= 1
+
+    if size < 3:
+        return [None, None]
+
+    sorted_list = heapsort(input_list, size)
+    num_1 = list()
+    num_2 = list()
+
+    while len(sorted_list) > 0:
+        if len(sorted_list) % 2 == 0:
+            num_1.append(str(sorted_list.pop()))
+        else:
+            num_2.append(str(sorted_list.pop()))
+    return [int(''.join(num_1)), int(''.join(num_2))]
 
 
 def test_function(test_case):
@@ -78,8 +85,111 @@ def test_function(test_case):
         print("Fail")
 
 
-#test_function([[1, 2, 3, 4, 5], [542, 31]])
-#test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
-#rearrange_digits([4, 6, 2, 5, 9, 8])
-case = [4, 6, 2, 5, 9, 8]
-print(heapsort(case, len(case)))
+cases = [[[1, 1, 2], [21, 1]], [[1, 2, 3, 4, 5], [
+    542, 31]], [[4, 6, 2, 5, 9, 8], [964, 852]], [[3,
+                                                   4,
+                                                   5,
+                                                   3,
+                                                   2,
+                                                   8,
+                                                   4,
+                                                   6,
+                                                   2,
+                                                   1,
+                                                   2,
+                                                   6,
+                                                   4,
+                                                   7,
+                                                   2,
+                                                   8,
+                                                   5,
+                                                   6,
+                                                   1,
+                                                   1,
+                                                   7,
+                                                   8,
+                                                   6,
+                                                   6,
+                                                   3,
+                                                   1,
+                                                   8,
+                                                   2,
+                                                   5,
+                                                   2,
+                                                   4,
+                                                   8,
+                                                   8,
+                                                   3,
+                                                   2,
+                                                   4,
+                                                   6,
+                                                   1,
+                                                   1,
+                                                   3,
+                                                   2,
+                                                   3,
+                                                   8,
+                                                   8,
+                                                   2,
+                                                   3,
+                                                   2,
+                                                   3,
+                                                   6,
+                                                   1,
+                                                   4,
+                                                   6,
+                                                   5,
+                                                   1,
+                                                   3,
+                                                   2,
+                                                   6,
+                                                   6,
+                                                   4,
+                                                   7,
+                                                   6,
+                                                   1,
+                                                   6,
+                                                   2,
+                                                   1,
+                                                   8,
+                                                   2,
+                                                   5,
+                                                   7,
+                                                   6,
+                                                   7,
+                                                   8,
+                                                   6,
+                                                   3,
+                                                   1,
+                                                   5,
+                                                   3,
+                                                   7,
+                                                   5,
+                                                   6,
+                                                   7,
+                                                   6,
+                                                   7,
+                                                   3,
+                                                   3,
+                                                   4,
+                                                   3,
+                                                   4,
+                                                   7,
+                                                   7,
+                                                   5,
+                                                   5,
+                                                   4,
+                                                   4,
+                                                   7,
+                                                   3,
+                                                   7,
+                                                   6,
+                                                   8,
+                                                   3],
+                                                  [88888877777766666666555554444433333333222222211111,
+                                                   88888777777666666666555544444433333333222222111111]]]
+
+for case in cases:
+    test_function(case)
+
+# https://en.wikipedia.org/wiki/Heapsort
