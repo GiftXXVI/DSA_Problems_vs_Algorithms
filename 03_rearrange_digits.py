ba82ebeb473f45175f77a3f926a8234307d4f3c2
GiftXXVI <gift.chimphonda@outlook.com>
2022-01-20 11:59:03 +0200

    Rearrange Brainstorm #3

--- 03_rearrange_digits.py
+++ 03_rearrange_digits.py
@@ -1,3 +1,25 @@
+def heapsort(input, size):
+    heapify(input, size)
+    end = size - 1
+    while end > 0:
+        temp = input[end]
+        input[0] = input[end]
+        input[end] = temp
+        end -= 1
+        sift_down(input, 0, end)
+
+
+def heapify(input, size):
+    start = ((size - 1) - 1) / 2
+    while start>0:
+        
+    pass
+
+
+def sift_down():
+    pass
+
+
 def rearrange_digits(input_list):
     """
     Rearrange Array Elements so as to form two number such that their sum is maximum.
@@ -8,12 +30,11 @@ def rearrange_digits(input_list):
        (int),(int): Two maximum sums
     """
     size = len(input_list)
-    sizes = (size//2, size-(size//2))
+    sizes = (size // 2, size - (size // 2))
     max_size = max(sizes)
     print(size, sizes, max_size)
     i = max_size
     while i > 0:
-        
         i -= 1
 
 
@@ -28,4 +49,6 @@ def test_function(test_case):
 
 #test_function([[1, 2, 3, 4, 5], [542, 31]])
 #test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
-rearrange_digits([4, 6, 2, 5, 9, 8])
+#rearrange_digits([4, 6, 2, 5, 9, 8])
+case = [4, 6, 2, 5, 9, 8]
+print(heapsort(case))
