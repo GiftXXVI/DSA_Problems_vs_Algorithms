d98c60d95d248b7bd312b3499f70beee47f22055
GiftXXVI <gift.chimphonda@outlook.com>
2022-01-23 12:36:51 +0200

    Started Route Handlers

--- 07_request_routing.py
+++ 07_request_routing.py
@@ -1,27 +1,43 @@
+class Handler():
+    def __init__(self, response, code):
+        self.response = response
+        self.code = code
+
+    def __repr__(self):
+        return f'{str(self.code)}: {self.response}'
+
+
 # A RouteTrie will store our routes and their associated handlers
+
+
 class RouteTrie:
     def __init__(self):
         # Initialize the trie with an root node and a handler, this is the root
         # path or home page node
         self.root = RouteTrieNode()
 
-    def insert(self, path):
+    def insert(self, path, handler):
         # Similar to our previous example you will want to recursively add nodes
         # Make sure you assign the handler to only the leaf (deepest) node of
         # this path
         node = self.root
-        for index, section in enumerate(path):
-            if section not in node.children:
-                node.insert(section)
-            node = node.children[section]
-        node.is_terminal = True
+        if self.find(path) is not None:
+            for index, section in enumerate(path):
+                if section not in node.children:
+                    node.insert(section)
+                node = node.children[section]
+            node.handler = Handler(handler, 200)
+            node.is_terminal = True
 
     def find(self, path):
         # Starting at the root, navigate the Trie to find a match for this path
         # Return the handler for a match, or None for no match
 
         node = self.root
+        if path[0] == node.value:
+            return node
         for index, section in enumerate(path):
+            print(node.value, node)
             if section not in node.children:
                 return None
             node = node.children[section]
@@ -34,35 +50,39 @@ class RouteTrie:
 
 
 class RouteTrieNode:
-    def __init__(self, value='/', terminal=False):
+    def __init__(self, value='/', terminal=False, handler=None):
         # Initialize the node with children as before, plus a handler
         self.is_terminal = terminal
         self.value = value
         self.children = dict()
+        self.handler = handler
 
     def insert(self, value):
         # Insert the node as before
         self.children[value] = RouteTrieNode(value=value)
 
     def __repr__(self):
-        return f'<RouteTrieNode value:{self.value},is_terminal:{self.is_terminal}>'
+        return f'<RouteTrieNode value:{self.value},is_terminal:{self.is_terminal},handler:{self.handler}>'
 
 
 # The Router class will wrap the Trie and handle
 
 
 class Router:
-    def __init__(self):
+    def __init__(self, root_handler, not_found_handler):
         # Create a new RouteTrie for holding our routes
         # You could also add a handler for 404 page not found responses as
         # well!
         self.trie = RouteTrie()
+        self.error_handler = Handler(not_found_handler, 404)
+        if root_handler is not None:
+            self.add_handler('/', root_handler)
 
-    def add_handler(self, path):
+    def add_handler(self, path, handler):
         # Add a handler for a path
         # You will need to split the path and pass the pass parts
         # as a list to the RouteTrie
-        trie.insert(self.split_path(path))
+        self.trie.insert(self.split_path(path), handler)
 
     def lookup(self, path):
         # lookup path (by parts) and return the associated handler
@@ -70,7 +90,11 @@ class Router:
         # return the "not found" handler if you added one
         # bonus points if a path works with and without a trailing slash
         # e.g. /about and /about/ both return the /about handler
-        trie.find(self.split_path(path))
+        node = self.trie.find(self.split_path(path))
+        if node is None:
+            print(self.error_handler)
+        else:
+            print(node.handler)
 
     def split_path(self, path):
         # you need to split the path into parts for
@@ -85,6 +109,7 @@ class Router:
         # remove the 'not found handler' if you did not implement this
 
 
+'''
 cases = [
     '/',
     '/features',
@@ -118,8 +143,8 @@ def test_function(test_case):
 
 for case in cases:
     test_function(case)
-
 '''
+
 router = Router("root handler", "not found handler")
 router.add_handler("/home/about", "about handler")  # add a route
 
@@ -132,4 +157,3 @@ print(router.lookup("/home/about"))  # should print 'about handler'
 print(router.lookup("/home/about/"))
 # should print 'not found handler' or None if you did not implement one
 print(router.lookup("/home/about/me"))
-'''
